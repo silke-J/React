1345b6551f12cb657e53eb352464fc4cad50ccea
silke-J <silk0776@mcdmstudent.dk>
2025-05-02 10:02:51 +0200

    useAuth

---
+++ react_postman/src/hoods/useAuth.jsx
@@ -0,0 +1,39 @@
+import { useState } from "react";
+
+const useAuth = () => {
+  const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+  //SignIn
+  const SignIn = async (signinData) => {
+    setIsLoading(true);
+    try {
+      const response = await fetch("http://localhost:3043/auth/signin", {
+        method: "POST",
+        body: {
+          email: {user.email},
+          password: {user.password},
+        },
+      });
+
+      if (!response.status === "error") {
+        throw new Error("Failed to signIn");
+      }
+
+      const result = await response.json();
+      return result;
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  return {
+    SignIn,
+    error,
+    isLoading,
+  };
+};
+
+export { useAuth };
