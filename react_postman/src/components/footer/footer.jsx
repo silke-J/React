bb41bf355d52c6f6103ed13452e6f1567079eab0
mijam31 <156896989+mijam31@users.noreply.github.com>
2025-05-06 11:06:31 +0200

    Footer

--- react_postman/src/components/footer/footer.jsx
+++ react_postman/src/components/footer/footer.jsx
@@ -1,10 +1,19 @@
 import logo from "../../../public/logo.png";
 import styles from "./footer.module.css";
+import { FaFacebookSquare } from "react-icons/fa";
+import { RiInstagramFill } from "react-icons/ri";
 
 const Footer = () => {
   return (
     <footer className={styles.footer}>
-      <img src={logo} alt="" />
+      <div className={styles.some}>
+        <FaFacebookSquare />
+        <RiInstagramFill />
+      </div>
+      <div className={styles.logo}>
+        <img src={logo} alt="" />
+        <p>Gittes Glamping</p>
+      </div>
     </footer>
   );
 };
